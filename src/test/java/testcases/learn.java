510cf212b339b9df8d376354e7ce4ad5c2a0e1fd
Anishsingh12345 <107606108+Anishsingh12345@users.noreply.github.com>
2024-12-18 14:20:29 +0530

    new

---
+++ src/test/java/testcases/learn.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class learn {
+
+
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("anish jatt sab sahi hai bhai//testng ");
+
+	}
+}
